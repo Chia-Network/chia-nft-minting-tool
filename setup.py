c2268418848ff5f25fc7bde47b9088cdf42f94ca
geoffwalmsley <geoffwalmsley@gmail.com>
2022-07-16 13:54:40 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "rt") as fh:
+    long_description = fh.read()
+
+dependencies = [
+    "chia-blockchain @ git+ssh://git@github.com/Chia-Network/chia-blockchain.git@gw.mint_from_did",
+    "packaging==21.3",
+    "pytest",
+    "pytest-asyncio",
+]
+
+dev_dependencies = [
+    "flake8",
+    "mypy",
+    "black",
+]
+
+setup(
+    name="chianft",
+    version="0.1",
+    packages=find_packages(exclude=("tests",)),
+    author="Geoff Walmsley",
+    entry_points={
+        "console_scripts": ["chianft = chianft.cmds.cli:main"],
+    },
+    package_data={
+        "": ["*.clvm", "*.clvm.hex", "*.clib", "*.clsp", "*.clsp.hex"],
+    },
+    author_email="g.walmsley@chia.net",
+    setup_requires=["setuptools_scm"],
+    install_requires=dependencies,
+    url="https://github.com/Chia-Network",
+    license="https://opensource.org/licenses/Apache-2.0",
+    description="Chia NFT minting toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Security :: Cryptography",
+    ],
+    extras_require=dict(
+        dev=dev_dependencies,
+    ),
+    project_urls={
+        "Bug Reports": "https://github.com/Chia-Network/chianft",
+        "Source": "https://github.com/Chia-Network/chianft",
+    },
+)
