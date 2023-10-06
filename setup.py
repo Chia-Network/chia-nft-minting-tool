df398d63dc280d4fabd9e940dce5453b6c44b1a1
Chris Marslender <chrismarslender@gmail.com>
2023-10-06 13:16:38 -0500

    chia-blockchain 2.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "rt", encoding="UTF-8") as fh:
     long_description = fh.read()
 
 dependencies = [
-    "chia-blockchain==2.0.0",
+    "chia-blockchain==2.1.0",
 ]
 
 dev_dependencies = [
