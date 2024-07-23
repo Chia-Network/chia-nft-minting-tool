38a0020664dfac27bd980b1ceb99426a91668a49
Adam Kelly <338792+aqk@users.noreply.github.com>
2024-07-23 05:13:37 -0700

    update chia-blockchain to 2.4.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "rt", encoding="UTF-8") as fh:
     long_description = fh.read()
 
 dependencies = [
-    "chia-blockchain==2.4.0",
+    "chia-blockchain==2.4.2",
 ]
 
 dev_dependencies = [
